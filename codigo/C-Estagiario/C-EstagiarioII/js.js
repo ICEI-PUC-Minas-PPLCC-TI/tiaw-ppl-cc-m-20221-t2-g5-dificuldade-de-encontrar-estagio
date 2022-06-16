f74f60b872a905bef834dc402f1ff7a41f98f256
FelipeParreiras <77993189+FelipeParreiras@users.noreply.github.com>
2022-06-16 10:47:53 -0300

    estagiarioIII

--- codigo/C-Estagiario/C-EstagiarioII/js.js
+++ codigo/C-Estagiario/C-EstagiarioII/js.js
@@ -3,3 +3,18 @@ const form = document.getElementById('formII')
 function exibir(){
     form.style.visibility="visible"
 }
+function getFormsIValue(event) {
+    event.preventDefault();
+    var form = document.getElementById("formII");
+    console.log(form.name.value)
+    if (form.instEnsino.value != "" && form.tipoFormacao.value != "" && form.andamento.value != "") {
+        var secondOne = {
+            "instEnsino": form.instEnsino.value,
+            "tipoFormacao": form.tipoFormacao.value,
+            "andamento": form.andamento.value,
+        }
+        window.location.href="../C-EstagiarioIII/index.html"
+    }else{
+        alert("Campos não preenchidos")
+    }
+}
