1cc3ca77ebcd62c3fa33740d751d8e51cba2befa
AugustoNoronha <augustonoronha03@gmail.com>
2022-06-16 12:16:34 -0300

    commit for validators login

--- codigo/Cadastro/C-Empresa/js.js
+++ codigo/Cadastro/C-Empresa/js.js
@@ -2,27 +2,28 @@ function avancar(event) {
     event.preventDefault();
     var form = document.getElementById("formI");
     console.log(form.name.value)
-    if (form.name.value != "" ) {
+    if (form.name.value != "" && form.cnpj.value != "") {
         const chkEmpresa = document.querySelector('#chkEmpresa:checked') !== null
         const chkMEI = document.querySelector('#chkMEI:checked') !== null
         const chkPofLib = document.querySelector('#chkPofLib:checked') !== null
         const chkPublic = document.querySelector('#chkPublic:checked') !== null
-
-        if (form.pass.value === form.confirmPass.value && form.pass.value != "" && form.confirmPass.value != "") {
+        if (chkEmpresa !== false || chkMEI !== false || chkPofLib !== false || chkPublic !== false) {
             var firstOne = {
                 "name": form.name.value,
-                "email": form.email.value,
-                "pass": form.pass.value,
-                "confimPass": form.confirmPass.value
-            }
-            
-            localStorage.setItem('firstOne',JSON.stringify(firstOne));
-            window.location.href="./C-EstagiarioII/index.html"
+                "cnpj": form.cnpj.value,
+                "chkEmpresa": chkEmpresa,
+                "chkMEI": chkMEI,
+                "chkPofLib": chkPofLib,
+                "chkPublic": chkPublic
 
-        } else {
-         alert("As senhas devem ser identicas")
+            }
+    
+            localStorage.setItem('firstOneEmpresas', JSON.stringify(firstOne));
+            window.location.href = "./C-EmpresaII/index.html"
+        }else{
+        alert("Indique o tipe de empresa!!")
         }
-    }else{
+    } else {
         alert("Campos invalidos")
     }
 }
