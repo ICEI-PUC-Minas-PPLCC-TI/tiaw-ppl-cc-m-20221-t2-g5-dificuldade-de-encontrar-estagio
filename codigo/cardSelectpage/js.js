fde0b369d0a71b349d7a10c52ec3f4ae875e55de
AugustoNoronha <augustonoronha03@gmail.com>
2022-07-01 11:31:55 -0300

    login path

--- codigo/cardSelectpage/js.js
+++ codigo/cardSelectpage/js.js
@@ -1,11 +1,21 @@
 
 function typeCadastro(type) {
+    let locate = localStorage.getItem('locate')
     localStorage.setItem('type', type)
-    if(type == 'Empresa'){
-        location.href = "../Cadastro/C-Empresa/index.html"
+    if(locate === "Entrar"){
+        if(type == 'Empresa'){
+            location.href = "../LoginPage/Empresa/indexEmpresa.html"
+        }else{
+            location.href="../LoginPage/Estagiario/indexEstagiario.html"
+        }
     }else{
-        location.href="../C-Estagiario/index.html"
+        if(type == 'Empresa'){
+            location.href = "../Cadastro/C-Empresa/index.html"
+        }else{
+            location.href="../C-Estagiario/index.html"
+        }
     }
+   
     
     console.log(type);
 }
