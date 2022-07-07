04fe8b50942a19dee6a9206977aa2cd87a8b3d31
AugustoNoronha <augustonoronha03@gmail.com>
2022-07-07 16:19:40 -0300

    commit

--- codigo/listar_empresas/js.js
+++ codigo/listar_empresas/js.js
@@ -1,152 +1,93 @@
-const array = [
-    {
-        'id': 1,
-        'nome': 'Coisa',
-        'turno': 'Manha',
-        'horas': '6',
-        'nomeVagas': 'Vaga1'
-    },
-    {
-        'id': 2,
-        'nome': 'Coisa1',
-        'turno': 'Tarde',
-        'horas': '6',
-        'nomeVagas': 'Vaga2'
-    },
-    {
-        'id': 3,
-        'nome': 'Coisa2',
-        'turno': 'Noite',
-        'horas': '4',
-        'nomeVagas': 'Vaga3'
-    },
-    {
-        'id': 4,
-        'nome': 'Coisa3',
-        'turno': 'Manha',
-        'horas': '6',
-        'nomeVagas': 'Vaga4'
-    }
-];
-const arrayall = [
-    {
-        'id': 1,
-        'nome': 'Coisa',
-        'turno': 'Manha',
-        'horas': '6',
-        'nomeVagas': 'Vaga1'
-    },
-    {
-        'id': 2,
-        'nome': 'Coisa1',
-        'turno': 'Tarde',
-        'horas': '6',
-        'nomeVagas': 'Vaga2'
-    },
-    {
-        'id': 3,
-        'nome': 'Coisa2',
-        'turno': 'Noite',
-        'horas': '4',
-        'nomeVagas': 'Vaga3'
-    },
-    {
-        'id': 4,
-        'nome': 'Coisa3',
-        'turno': 'Manha',
-        'horas': '6',
-        'nomeVagas': 'Vaga4'
-    },
-    {
-        'id': 5,
-        'nome': 'Coisa6',
-        'turno': 'Manha',
-        'horas': '4',
-        'nomeVagas': 'Vaga8'
-    },
-    {
-        'id': 6,
-        'nome': 'Coisa7',
-        'turno': 'Tarde',
-        'horas': '8',
-        'nomeVagas': 'Vaga7'
-    },
-];
+
 window.onload = () => {
-    coisa();
-    getAllVagas();
+    getData();
 }
 
-function coisa() {
-    let vagasContainer = document.querySelector("#daniel")
-    vagasContainer.innerHTML = ""
-    array.forEach((data) => {
-        let id = data.id
-        let titulo = data.nome
-        let nomeVaga = data.nomeVagas
-        let horas = data.horas
-        let turno = data.turno
-        let container = `
-        <div class="containerCard" data-id="${id}>
-            <div class="main" id="main">
-                <div id="title">
-                  <h1 class="titleh1">${titulo}</h1>
-                 </div>
-                 <div id="container2">
 
-                     <h3 class="nomeVaga">Nome da vaga: ${nomeVaga}</h3>
-           
-                    <h3 class="horas">Horas diarias: ${horas}</h3>
-                    <h3 class="turno">Turno de trabalho: ${turno}</h3>
-                 </div>
-                 
-            </div>
-        </div>`;
-        vagasContainer.innerHTML += container;
-    })
 
-}
-function getAllVagas() {
+function getData() {
+    var first = {
+        "primeiro": JSON.parse(localStorage.getItem("novavaga"))
+    }
+    var tudo = []
+    tudo.push(first)
     let vagasContainer = document.querySelector("#allvagas")
     vagasContainer.innerHTML = ""
-    arrayall.forEach((data) => {
-        let id = data.id
-        let titulo = data.nome
-        let nomeVaga = data.nomeVagas
-        let horas = data.horas
-        let turno = data.turno
-        let container = `
-        <div class="containerCard" data-id="${id}>
-            <div class="main" id="main">
-                <div id="title">
-                  <h1 class="titleh1">${titulo}</h1>
-                 </div>
-                 <div id="container2">
+    tudo.forEach((data) => {
+        data.primeiro.forEach((data) => {
+            let id = data.id;
+            let nomeVaga = data.nome
+            let horas = data.horasdiarias
+            let turnoM = data.turnoM
+            let turnoN = data.turnoN
+            let turnoT = data.turnoT
+            let desc = data.descricao
+            let atuacao = data.atuacao
+            var turno;
 
-                     <h3 class="nomeVaga">Nome da vaga: ${nomeVaga}</h3>
-           
-                    <h3 class="horas">Horas diarias: ${horas}</h3>
-                    <h3 class="turno">Turno de trabalho: ${turno}</h3>
-                 </div>
-                 
-            </div>
-        </div>`;
-        vagasContainer.innerHTML += container;
+            if (turnoM == true) {
+                turno = 'Manhã'
+            } else if (turnoT == true) {
+                turno = 'Tarde'
+            } else {
+                turno = "Noite"
+            }
+
+
+            let container = `
+            <div class="containerCard" data-id="${id}>
+                <div class="main" id="main">
+                    <div id="title">
+                      <h1 class="titleh1">Vagas</h1>
+                     </div>
+                     <div id="container2">
+    
+                         <h3 class="nomeVaga">Nome da vaga: ${nomeVaga}</h3>
+    
+                        <h3 class="horas">Horas diarias: ${horas}</h3>
+                        <h3 class="turno">Turno de trabalho: ${turno}</h3>
+                        <p class="desc">Descrição da vaga: ${desc}</p>
+                        <p class="atuação">Area de atuação: ${atuacao}</p>
+    
+                     </div>
+                     
+                </div>
+            </div>`;
+            vagasContainer.innerHTML += container;
+        })
+
     })
 
 }
 
 function novavagaSaveValue() {
     var form = document.getElementById("novavaga")
+    const ipmanha = document.querySelector('#ipmanha:checked') !== null
+    const iptarde = document.querySelector('#iptarde:checked') !== null
+    const ipnoite = document.querySelector('#ipnoite:checked') !== null
+
     var novavaga = {
+        "id": 0,
         "nome": form.ipnome.value,
-        "turnoM": form.ipmanha.value,
-        "turnoT": form.iptarde.value,
-        "turnoN": form.ipnoite.value,
+        "turnoM": ipmanha,
+        "turnoT": iptarde,
+        "turnoN": ipnoite,
         "atuacao": form.ipatuacao.value,
         "horasdiarias": form.iphoras.value,
         "descricao": form.ipdescricao.value,
     }
-    localStorage.setItem("novavaga", JSON.stringify(novavaga))
+    if (localStorage.getItem('novavaga') === null) {
+        localStorage.setItem("novavaga", JSON.stringify([novavaga]))
+    } else {
+        localStorage.setItem(
+            'novavaga', JSON.stringify([
+                ...JSON.parse(localStorage.getItem('novavaga')),
+                novavaga
+            ])
+        );
+
+    }
+    alert("Vaga criada com sucesso")
+    window.location.reload(true)
     console.log(novavaga)
 }
